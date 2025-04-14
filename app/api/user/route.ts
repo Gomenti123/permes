0b5e447003b12c3fa6fe39984843e9f0d91e8c95
MAC <user@MACs-MacBook-Air.local>
2025-04-14 11:10:17 -0700

    update

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -2,7 +2,7 @@ import { dbConfig } from "@/app/utils/dbConfig"
 import myUserModel from "@/app/utils/model/userModel"
 import { NextRequest, NextResponse } from "next/server"
 
-export const POST = async (req:NextRequest, res:NextResponse)=>{
+export const POST = async (req:NextRequest)=>{
     try {
         await dbConfig()
         const {username, name} = await req.json()
