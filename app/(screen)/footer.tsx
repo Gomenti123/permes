621d0cedcaf92582a43fede297d79899836446b1
MAC <user@MACs-MacBook-Air.local>
2025-04-14 15:48:40 -0700

    change of footer color

--- app/(screen)/footer.tsx
+++ app/(screen)/footer.tsx
@@ -3,7 +3,7 @@ import { SlShare } from "react-icons/sl";
 
 const Footer = () => {
   return (
-    <div className="bg-green-600 w-full p-2 flex justify-between ">
+    <div className="bg-yellow-700 w-full p-2 px-[30px] flex justify-between ">
       <div>
         <SlShare className="text-white font-bold text-[30px]" />
         <p className="text-white text-[12px]">Share</p>
