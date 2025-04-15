fd7c3e27ac57fbc81900203b33f8879c4b30dd7e
MAC <user@MACs-MacBook-Air.local>
2025-04-15 16:55:16 -0700

    update

--- app/global/storeProvider.tsx
+++ app/global/storeProvider.tsx
@@ -9,7 +9,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>();
+  const storeRef = useRef<any>(0);
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
