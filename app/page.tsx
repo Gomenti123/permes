6918524ed0c370bd73bb08947931ead93210cf6c
MAC <user@MACs-MacBook-Air.local>
2025-04-16 06:41:35 -0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,14 @@ const page = () => {
     <div>
       This is the main page
       <div className="fixed bottom-10 right-5 animate-bounce">
-        <a href={`https://wa.me/2349045190274?text=${text}`}>
-          <BsWhatsapp className="text-green-500 text-[50px]" />
+        <a
+          className="flex flex-col gap-2 justify-center items-center"
+          href={`https://wa.me/2349045190274?text=${text}`}
+        >
+          <BsWhatsapp className="text-green-500 text-[50px] shadow-green-400 shadow-2xl p-2 rounded-full" />
+          <p className="text-[17px] font-semibold text-green-600">
+            Request Pickup
+          </p>
         </a>
       </div>
     </div>
