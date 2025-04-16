39458401d7c411ea683414e3f12bb0c5cb65d896
MAC <user@MACs-MacBook-Air.local>
2025-04-15 17:26:01 -0700

    navigation on referral page

--- app/(screen)/footer.tsx
+++ app/(screen)/footer.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import { HiHome } from "react-icons/hi";
 import { SlShare } from "react-icons/sl";
+import { useSelector } from "react-redux";
 
 const Footer = () => {
+  const user = useSelector((state: any) => {
+    return state.user;
+  });
   return (
     <div className="bg-yellow-700 w-full p-2 px-[30px] flex justify-between ">
       <div className="flex flex-col justify-center gap-1">
@@ -13,7 +18,7 @@ const Footer = () => {
         </Link>
       </div>
       <div className="flex flex-col items-center gap-1">
-        <Link href={"/"}>
+        <Link href={`/referral/${user._id}/share`}>
           <SlShare className="text-white font-bold text-[30px]" />
           <p className="text-white text-[12px]">Share</p>
         </Link>
---
+++ app/(screen)/referral/[userID]/share/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Share = () => {
+  return <div>This is the share page</div>;
+};
+
+export default Share;
