73102e975a76126907cccbca0c59112cdd95beb4
MAC <user@MACs-MacBook-Air.local>
2025-04-16 09:03:04 -0700

    update

--- app/(static)/Toogle.tsx
+++ app/(static)/Toogle.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Toogle = () => {
   return (
-    <div className="h-screen bg-neutral-600 w-full fixed">
+    <div className="h-screen bg-neutral-600 w-full mt-[-10px] fixed">
       <div className="px-[20px] py-10 flex flex-col gap-3">
         <nav className="cursor-pointer font-semibold bg-amber-500 p-2 w-full text-white rounded-md text-center">
           How This Works
