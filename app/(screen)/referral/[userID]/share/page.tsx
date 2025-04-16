4c7a704ca24b7b27e4684d8838ab792232a3b47f
MAC <user@MACs-MacBook-Air.local>
2025-04-15 18:27:45 -0700

    update on whatsapp sharing

--- app/(screen)/referral/[userID]/share/page.tsx
+++ app/(screen)/referral/[userID]/share/page.tsx
@@ -1,9 +1,17 @@
+"use client";
 import React from "react";
+import { WhatsappShareButton, WhatsappIcon } from "react-share";
 
 const Share = () => {
   return (
-    <div className="flex justify-center pt-5">
+    <div className="flex flex-col items-center gap-10 justify-center pt-5">
       <div>This is the share page</div>
+
+      <div>
+        <WhatsappShareButton url="https://permes-uutn.vercel.app/">
+          <WhatsappIcon className="rounded-full" />
+        </WhatsappShareButton>
+      </div>
     </div>
   );
 };
