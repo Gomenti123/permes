ea1fee3cae69406c4a8bcad236dfe3454b040ba3
MAC <user@MACs-MacBook-Air.local>
2025-04-15 17:28:45 -0700

    update

--- app/(screen)/footer.tsx
+++ app/(screen)/footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
   return (
     <div className="bg-yellow-700 w-full p-2 px-[30px] flex justify-between ">
       <div className="flex flex-col justify-center gap-1">
-        <Link href={"/referral/:userID"}>
+        <Link href={`/referral/${user._id}`}>
           <HiHome className="text-white font-bold text-[30px]" />
           <p className="text-white text-[12px]">Dashboard</p>
         </Link>
