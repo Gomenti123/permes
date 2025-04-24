876e0bc627e8ff5b5d6654a234946b9f921e59f3
MAC <user@MACs-MacBook-Air.local>
2025-04-24 15:10:54 -0700

    update

--- app/(screen)/header.tsx
+++ app/(screen)/header.tsx
@@ -1,13 +1,12 @@
+import Image from "next/image";
 import React from "react";
+import img from "../../public/logo2.png";
 
 const Header = () => {
   return (
-    <div className="flex flex-col gap-3 ">
-      <div className="flex gap-1 md:gap-4 pt-2  items-center">
-        <h1 className="pl-[26px] text-[15px] md:text-[20px] font-semibold">
-          Permes Laundry
-        </h1>
-        <p className="text-[9px]"> Easy loans for your every need 🌟</p>
+    <div className="flex flex-col gap-1 md:gap-3 ">
+      <div className="flex gap-1 md:gap-4 p-3 md:py-5 md:px-10 items-center">
+        <Image src={img} alt="#" className="w-[150px] md:w-[200px]" />
       </div>
       <div className="border border-neutral-400 w-full"></div>
     </div>
